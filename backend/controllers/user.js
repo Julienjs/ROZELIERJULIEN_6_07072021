46387a4760d66615e721c1c52b97114250da1337
Rozelier Julien <rozelierjjs@gmail.com>
2021-11-12 11:30:44 +0100

    finit

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -32,7 +32,7 @@ exports.login = (req, res, next) => {
                         userId: user._id,
                         token: jwt.sign(
                             { userId: user._id },
-                            process.env.DBTOKEN,
+                            process.env.JWT_SECRET,
                             { expiresIn: '24h' }
                         )
                     });
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: './.env' });
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, process.env.DBTOKEN);
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
         const userId = decodedToken.userId;
         if (req.body.userId && req.body.userId !== userId) {
             throw 'Invalid user ID';
